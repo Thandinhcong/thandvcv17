5a0310a3653474a371a9349c2ac51fb56064a836
thandinhjs <dinhcongthan03@gmail.com>
2023-05-08 23:16:14 +0700

    update

--- assaiment/components/header/logo.js
+++ assaiment/components/header/logo.js
@@ -1,6 +1,6 @@
 const logo = () => {
     return `
-    <div class="logo text-danger fs-2 p-4">MyCV</div>
+    <a href="/"><div class="logo text-danger fs-2 p-4">MyCV</div></a>
     `;
 }
 export default logo;
