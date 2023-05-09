8c5b1bc97ab6332dda3ef4b1afb9fe4d9647e10a
thandinhjs <dinhcongthan03@gmail.com>
2023-05-09 17:34:04 +0700

    Update

--- assaiment/components/main/education.js
+++ assaiment/components/main/education.js
@@ -6,7 +6,7 @@ const ListEdu = () => {
    <div class="edu text-muted ms-3">
    <h4>Cao đẳng FPT Polytechnic</h4>
    <li>Học tập/làm việc cùng với các bạn  để cải thiện các dự án trên lớp và dự án cá nhân</li>
-   <li>Kiến thức nền tảng về PHP,HTML,CSS,JavaScript</li>
+   <li>Kiến thức nền tảng về PHP(basic),HTML,CSS,JavaScript,React-Typescript(basic),NodeJS(basic),Angular(basic)</li>
    <li>Nhận thức về ngành phát triển web và các công nghệ mới</li>
    </div>
     </div>
